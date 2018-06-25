71d85201d713ca862d95adca6e441616df500753
James Bovis <hello@jamesbovis.com>
2018-06-25 19:44:40 +0100

    Remove unused js files

--- js/loadOnScroll.js
+++
@@ -1,8 +0,0 @@
-var wow = new WOW(
-  {
-    boxClass: 'colour-img', // which element will be animated
-    animateClass: 'coloured', // class that is added when in view
-    offset: 250 // distance to the element
-  }
-);
-wow.init();
