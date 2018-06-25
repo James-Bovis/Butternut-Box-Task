3f677089e5c9477280478b4c9f5869335d0c610a
James Bovis <hello@jamesbovis.com>
2018-06-25 20:40:00 +0100

    Add image compresser task to reduce SVG's

--- gulpfile.js
+++ gulpfile.js
@@ -3,41 +3,48 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var runSequence = require('run-sequence');
 var del = require('del');
+var cache = require('gulp-cache');
+var imagemin = require('gulp-imagemin');
 
-// TASKS FOR RUNNING LOCAL BUILD
-
-  // Runs browsersync on src folder
-  gulp.task('browserSync', function() {
-    browserSync.init({
-      server: {
-        baseDir: './',
-      },
-    })
+// Runs browsersync on src folder
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
   })
+})
 
-  // Compiles the SCSS sheets code into CSS in SRC
-  gulp.task('sass', function(){
-    return gulp.src('sass/style.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('css/'))
-      .pipe(browserSync.reload({
-        stream: true
-      }))
-  });
+// Compiles the SCSS into CSS
+gulp.task('sass', function(){
+  return gulp.src('sass/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
 
-  // Watches CSS and JS files for changes and reloads browser
-  gulp.task('watch', ['browserSync', 'sass'], function (){
-    gulp.watch('sass/**/*.scss', ['sass']); 
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload); 
-    gulp.watch('js/**/*.js', browserSync.reload); 
-  });
+// Watches CSS, JS and HTML files for changes and reloads browser
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('sass/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload); 
+  gulp.watch('js/**/*.js', browserSync.reload); 
+});
 
-  // Main Gulp Task
-  gulp.task('default', function (callback) {
-    runSequence(['sass','browserSync', 'watch'],
-      callback
-    )
-  })
+// Compress images
+gulp.task('images', function(){
+  return gulp.src('img/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin())) // Use cache to save on time
+  .pipe(gulp.dest('img/'))
+});
+
+// Main Gulp Task
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+})
 
 
